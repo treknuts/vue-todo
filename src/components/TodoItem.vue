6f3ce9c5f3f91387d0aca261d385f059d50f77b7
treknuts <trevor.a.knutson@ndsu.edu>
2020-08-31 10:33:41 -0500

    Initial commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>{{ todo.name }}</li>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ['todo']
+}
+</script>
+
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <h3>Shit fuck TODO list</h3>
+    <ul>
+      <TodoItem v-for="todoItem in todoList"
+        v-bind:key="todoItem.id"
+        v-bind:todo="todoItem">
+      </TodoItem>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TodoItem from "@/components/TodoItem.vue";
 
 export default {
   name: "Home",
+  // data function should likely make API calls for data
+  data: function () {
+        return {
+          todoList: [
+            { id: 1, name: "Learn Vue" },
+            { id: 2, name: "Build cool apps" },
+            { id: 3, name: "Get wins in Fall Guys" }
+        ]
+        }
+  },
   components: {
-    HelloWorld
+    TodoItem
   }
 };
 </script>
