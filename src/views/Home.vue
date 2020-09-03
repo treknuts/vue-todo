d2217ad7406d4987d728c5146d727f1044f0dda1
treknuts <49407165+treknuts@users.noreply.github.com>
2020-09-02 21:53:04 -0500

    Implemented drag and drop functionality on todo items

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,26 @@
 <template>
   <div class="home">
     <h3>Shit fuck TODO list</h3>
-    <div class="not-started">
-      <ul>
-        <draggable v-model="notStarted">
-          <TodoItem
-            v-for="todoItem in notStarted"
-            v-bind:key="todoItem.id"
-            v-bind:todo="todoItem"
-          />
-        </draggable>
-      </ul>
+    <h3>Not Started</h3>    
+    <div class="todo-bucket">
+    <draggable v-model="notStarted" group="people" @start="drag=true" @end="drag=false">
+          <TodoItem v-for="todoItem in notStarted" v-bind:key="todoItem.id" v-bind:todo="todoItem" />
+    </draggable>
     </div>
-    <div class="todo-buckets">
-      <div class="in-progress">
-        <ul>
-          <draggable v-model="inProgress">
-            <TodoItem
-              v-for="todoItem in inProgress"
-              v-bind:key="todoItem.id"
-              v-bind:todo="todoItem"
-            />
-          </draggable>
-        </ul>
-      </div>
-      <div class="done">
-        <ul>
-          <draggable v-model="done">
-            <TodoItem
-              v-for="todoItem in done"
-              v-bind:key="todoItem.id"
-              v-bind:todo="todoItem"
-            >
-            </TodoItem>
-          </draggable>
-        </ul>
-      </div>
+    <div class="todo-bucket">
+      <h3>In Progress</h3>
+      <div>
+      <draggable v-model="inProgress" group="people" @start="drag=true" @end="drag=false">
+          <TodoItem v-for="todoItem in inProgress" v-bind:key="todoItem.id" v-bind:todo="todoItem" />
+      </draggable>
     </div>
+      <h3>Done xD</h3>
+      <div class="todo-bucket">
+      <draggable v-model="done" group="people" @start="drag=true" @end="drag=false">
+          <TodoItem v-for="todoItem in done" v-bind:key="todoItem.id" v-bind:todo="todoItem" />
+      </draggable>
+      </div>
+  </div>
   </div>
 </template>
 
@@ -52,23 +36,25 @@ export default {
     return {
       notStarted: [{ id: 2, title: "Build cool apps", priority: "Medium" }],
       inProgress: [{ id: 1, title: "Learn Vue", priority: "High" }],
-      done: [{ id: 3, title: "Win in Fall Guys", priority: "Low" }],
+      done: [{ id: 3, title: "Win in Fall Guys", priority: "Low" }]
     };
   },
   components: {
     TodoItem,
-    draggable,
+    draggable
   },
   methods: {
     debug() {
       console.log(this.todoList);
-    },
-  },
+    }
+  }
 };
 </script>
 <style scoped>
-.todo-buckets {
-  display: flex;
-  flex-flow: row nowrap;
+.todo-bucket {
+  margin: 10px;
+  border-style: solid;
+  border-width: 2px;
+  min-height: 100px;
 }
 </style>
