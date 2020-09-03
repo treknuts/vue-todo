39ca1ccf851b74c05ca4c6206ffe39533dc8ad95
treknuts <trevor.a.knutson@ndsu.edu>
2020-09-03 13:48:36 -0500

    Styling todo buckets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,61 @@
 <template>
   <div class="home">
     <h3>Shit fuck TODO list</h3>
-    <h3>Not Started</h3>    
-    <div class="todo-bucket">
-    <draggable v-model="notStarted" group="people" @start="drag=true" @end="drag=false">
-          <TodoItem v-for="todoItem in notStarted" v-bind:key="todoItem.id" v-bind:todo="todoItem" />
-    </draggable>
-    </div>
-    <div class="todo-bucket">
-      <h3>In Progress</h3>
-      <div>
-      <draggable v-model="inProgress" group="people" @start="drag=true" @end="drag=false">
-          <TodoItem v-for="todoItem in inProgress" v-bind:key="todoItem.id" v-bind:todo="todoItem" />
-      </draggable>
-    </div>
-      <h3>Done xD</h3>
-      <div class="todo-bucket">
-      <draggable v-model="done" group="people" @start="drag=true" @end="drag=false">
-          <TodoItem v-for="todoItem in done" v-bind:key="todoItem.id" v-bind:todo="todoItem" />
-      </draggable>
+    <div class="content">
+      <div class="bucket">
+        <h4>Not Started</h4>
+        <div class="buckets">
+          <draggable
+            v-model="notStarted"
+            group="people"
+            @start="drag = true"
+            @end="drag = false"
+            class="todo-bucket"
+            v-on:change="debug"
+          >
+            <TodoItem
+              v-for="todoItem in notStarted"
+              v-bind:key="todoItem.id"
+              v-bind:todo="todoItem"
+            />
+          </draggable>
+        </div>
       </div>
-  </div>
+      <div class="bucket">
+        <h4>In Progress</h4>
+        <draggable
+          v-model="inProgress"
+          group="people"
+          @start="drag = true"
+          @end="drag = false"
+          class="todo-bucket"
+          v-on:change="debug"
+        >
+          <TodoItem
+            v-for="todoItem in inProgress"
+            v-bind:key="todoItem.id"
+            v-bind:todo="todoItem"
+          />
+        </draggable>
+      </div>
+      <div class="bucket">
+        <h4>Done xD</h4>
+        <draggable
+          v-model="done"
+          group="people"
+          @start="drag = true"
+          @end="drag = false"
+          class="todo-bucket"
+          v-on:change="debug"
+        >
+          <TodoItem
+            v-for="todoItem in done"
+            v-bind:key="todoItem.id"
+            v-bind:todo="todoItem"
+          />
+        </draggable>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,25 +71,42 @@ export default {
     return {
       notStarted: [{ id: 2, title: "Build cool apps", priority: "Medium" }],
       inProgress: [{ id: 1, title: "Learn Vue", priority: "High" }],
-      done: [{ id: 3, title: "Win in Fall Guys", priority: "Low" }]
+      done: [{ id: 3, title: "Win in Fall Guys", priority: "Low" }],
     };
   },
   components: {
     TodoItem,
-    draggable
+    draggable,
   },
   methods: {
     debug() {
-      console.log(this.todoList);
-    }
-  }
+      console.log(this.inProgress);
+      console.log(this.notStarted);
+      console.log(this.done);
+    },
+  },
 };
 </script>
 <style scoped>
+.content {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+}
+.bucket {
+  border-style: solid;
+  border-width: 1px;
+  border-color: gray;
+  border-radius: 5px;
+  margin: 20px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+}
 .todo-bucket {
   margin: 10px;
-  border-style: solid;
-  border-width: 2px;
   min-height: 100px;
+  max-width: 250px;
+  min-width: 100px;
 }
 </style>
